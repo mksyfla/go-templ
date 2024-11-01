f4f3d5c6c6dc490625bceff348451b94fb687944
Kasyfil <mksyfla@gmail.com>
2024-11-01 08:04:26 +0700

    add: templ attribute

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"go-jwp-emading/templates"
+	"log"
+	"net/http"
+)
+
+func main() {
+	attributes := templates.Page()
+	empty := templates.Empty()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		attributes.Render(r.Context(), w)
+	})
+
+	mux.HandleFunc("/404", func(w http.ResponseWriter, r *http.Request) {
+		empty.Render(r.Context(), w)
+	})
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Panic(err.Error())
+	}
+}
